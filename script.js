d4dbc64c14a845290409538aa58f9b007f4a21d1
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-06-30 12:19:29 +0530

    create function Upper

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("input")
+const output = document.getElementById("output")
+
+function Upper()
+{
+    const text = input.value
+    const res = output
+    res.innerText = text.toUpperCase()
+}
+
+function Lower()
+{
+    
+}
