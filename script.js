a01d1f2dabba7144ac8bb88125d3d5979fe07fa4
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-06-30 14:24:24 +0530

    add if-else ladder

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.getElementById("input")
 const output = document.getElementById("output")
 
-function Upper()
+/*function Upper()
 {
     const text = input.value
     const res = output
@@ -26,7 +26,23 @@ function Wordcnt()
 {
     const text = input.value
     const arr = text.split(" ")
-    
     const res = output
     res.innerText =`Word Count Is ${arr.length}`
+}*/
+
+function Action(event) {
+
+    if (event == 'upper') {
+        output.innerText = input.value.toUpperCase()
+    }
+    else if (event == 'lower') {
+        output.innerText = input.value.toLowerCase()
+    }
+    else if (event == 'charcnt') {
+        output.innerText = `The Length is ${input.value.length}`
+    }
+    else{
+        const arr = input.value.split(" ")
+        output.innerText = `Word Count Is ${arr.length}`
+    }
 }
